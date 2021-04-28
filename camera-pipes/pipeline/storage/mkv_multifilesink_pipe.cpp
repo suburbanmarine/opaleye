ce85ac8e6a027a35f172464cd25b50e67336cb9c
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2021-04-27 21:57:27 -0700

    multifilesink

--- camera-pipes/pipeline/storage/mkv_multifilesink_pipe.cpp
+++ camera-pipes/pipeline/storage/mkv_multifilesink_pipe.cpp
@@ -5,15 +5,20 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/fmt.h>
 
-mkv_multifilesink_pipe::mkv_multifilesink_pipe()
+mkv_multifilesink_pipe::mkv_multifilesink_pipe()/* : m_got_eos(false)*/
 {
   
 }
 
 void mkv_multifilesink_pipe::add_to_bin(const Glib::RefPtr<Gst::Bin>& bin)
 {
-  bin->add(m_bin);
+    bin->add(m_bin);
 }
+void mkv_multifilesink_pipe::remove_from_bin(const Glib::RefPtr<Gst::Bin>& bin)
+// {
+//     bin->remove(m_bin);
+// }
+
 bool mkv_multifilesink_pipe::link_front(const Glib::RefPtr<Gst::Element>& node)
 {
   node->link(m_in_queue);
@@ -24,11 +29,22 @@ bool mkv_multifilesink_pipe::link_back(const Glib::RefPtr<Gst::Element>& node)
   return false;
 }
 
+bool mkv_multifilesink_pipe::unlink_front(const Glib::RefPtr<Gst::Element>& node)
+{
+    node->unlink(m_in_queue);
+    return true;
+}
+
 bool mkv_multifilesink_pipe::init(const char name[])
 {
   //init our internal bin and elements
   {
     m_bin = Gst::Bin::create(fmt::format("{:s}-bin", name).c_str());
+
+    if( ! m_in_pipe.init("src") )
+    {
+        return false;
+    }
 
     m_in_queue    = Gst::Queue::create();
     m_in_queue->property_max_size_buffers()      = 0;
@@ -67,7 +83,65 @@ bool mkv_multifilesink_pipe::init(const char name[])
 
     m_in_queue->link(m_matroskamux);
     m_matroskamux->link(m_multifilesink);
+
+    // m_matroskamux->add_probe(GST_PAD_PROBE_TYPE_EVENT_DOWNSTREAM, sigc::mem_fun(&mkv_multifilesink_pipe::on_pad_probe, this))
   }
 
   return true;
 }
+
+// void mkv_multifilesink_pipe::send_eos()
+// {
+//   Glib::RefPtr<Gst::EventEos> eos = Gst::EventEos::create();
+//   m_in_queue->send_event(eos);
+// }
+
+// void mkv_multifilesink_pipe::install_wait_for_eos()
+// {
+//     m_got_eos = false;
+//     m_eos_probe = m_matroskamux->get_static_pad("sink")->add_probe(Gst::PAD_PROBE_TYPE_BLOCK | Gst::PAD_PROBE_TYPE_EVENT_DOWNSTREAM, sigc::mem_fun(*this, &mkv_multifilesink_pipe::on_pad_probe));
+// }
+// void mkv_multifilesink_pipe::wait_for_eos()
+// {
+//     std::unique_lock<std::mutex> lock(m_mutex_got_eos);
+//     m_cv_got_eos.wait(lock, [this]{return m_got_eos.load();});
+// }
+// void mkv_multifilesink_pipe::remove_wait_for_eos()
+// {
+//     if(m_eos_probe)
+//     {
+//         m_matroskamux->get_static_pad("sink")->remove_probe(m_eos_probe);
+//     }
+// }
+
+// Gst::PadProbeReturn mkv_multifilesink_pipe::on_pad_probe(const Glib::RefPtr<Gst::Pad>& pad, const Gst::PadProbeInfo& probe_info)
+// {
+//     Glib::RefPtr<Gst::Event> event = probe_info.get_event();
+//     if(!event)
+//     {
+//         return Gst::PAD_PROBE_OK;
+//     }
+
+//     Gst::PadProbeReturn ret = Gst::PAD_PROBE_OK;
+
+//     switch(event->get_event_type())
+//     {
+//         case GST_EVENT_EOS:
+//         {
+//             remove_wait_for_eos();
+  
+//             unlink_front(m_h264->back());
+
+//             m_got_eos = true;
+//             ret = Gst::PAD_PROBE_DROP;
+//             break;
+//         }
+//         default:
+//         {
+//             ret = Gst::PAD_PROBE_OK;
+//             break;
+//         }
+//     }
+
+//     return ret;
+// }
--- camera-pipes/pipeline/storage/mkv_multifilesink_pipe.hpp
+++ camera-pipes/pipeline/storage/mkv_multifilesink_pipe.hpp
@@ -2,9 +2,15 @@
 
 #include "pipeline/GST_element_base.hpp"
 
+#include "pipeline/GST_interpipesrc.hpp"
+
 #include <gstreamermm/caps.h>
 #include <gstreamermm/capsfilter.h>
 #include <gstreamermm/queue.h>
+
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
 
 class mkv_multifilesink_pipe : public GST_element_base
 {
@@ -12,8 +18,12 @@ public:
   mkv_multifilesink_pipe();
 
   void add_to_bin(const Glib::RefPtr<Gst::Bin>& bin) override;
+  // void remove_from_bin(const Glib::RefPtr<Gst::Bin>& bin) override;
+
   bool link_front(const Glib::RefPtr<Gst::Element>& node) override;
   bool link_back(const Glib::RefPtr<Gst::Element>& node) override;
+
+  bool unlink_front(const Glib::RefPtr<Gst::Element>& node) override;
 
   bool init(const char name[]) override;
 
@@ -22,11 +32,32 @@ public:
     return m_in_queue;
   }
 
+  Glib::RefPtr<Gst::Bin> get_bin()
+  {
+    return m_bin;
+  }  
+
+  // void send_eos();
+
+  // void install_wait_for_eos();
+  // void wait_for_eos();
+  // void remove_wait_for_eos();
+
 protected:
 
+  Gst::PadProbeReturn on_pad_probe(const Glib::RefPtr<Gst::Pad>& pad, const Gst::PadProbeInfo& probe_info);
+
   Glib::RefPtr<Gst::Bin>        m_bin;
+
+  GST_interpipesrc              m_in_pipe;
 
   Glib::RefPtr<Gst::Queue>      m_in_queue;
   Glib::RefPtr<Gst::Element>    m_matroskamux;
   Glib::RefPtr<Gst::Element>    m_multifilesink;
+
+  // gulong                        m_eos_probe;
+  
+  // std::atomic<bool>             m_got_eos;
+  // std::mutex                    m_mutex_got_eos;
+  // std::condition_variable       m_cv_got_eos;
 };
