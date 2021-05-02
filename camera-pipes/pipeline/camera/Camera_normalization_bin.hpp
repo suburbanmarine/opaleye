120e990f547871734ba2225cad64caf8f1f73e01
Jacob Schloss <jacob@schloss.io>
2021-05-01 18:54:22 -0700

    start refactoring video normal and framegrab

---
+++ camera-pipes/pipeline/camera/Camera_normalization_bin.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "pipeline/GST_element_base.hpp"
+
+#include <gstreamermm/caps.h>
+#include <gstreamermm/capsfilter.h>
+#include <gstreamermm/queue.h>
+#include <gstreamermm/tee.h>
+
+#include <atomic>
+#include <memory>
+#include <mutex>
+
+class Camera_normalization_bin : public GST_element_base
+{
+public:
+  Camera_normalization_bin();
+
+  void add_to_bin(const Glib::RefPtr<Gst::Bin>& bin) override;
+  bool link_front(const Glib::RefPtr<Gst::Element>& node) override;
+  bool link_back(const Glib::RefPtr<Gst::Element>& node) override;
+
+  Glib::RefPtr<Gst::Element> back() override
+  {
+    return m_out_tee;
+  }
+
+  bool init(const char name[]) override;
+
+protected:
+
+  Glib::RefPtr<Gst::Bin>        m_bin;
+
+  Glib::RefPtr<Gst::Queue>      m_in_queue;
+  Glib::RefPtr<Gst::Element>    m_videorate;
+  Glib::RefPtr<Gst::Caps>       m_out_caps;
+  Glib::RefPtr<Gst::CapsFilter> m_out_capsfilter;
+  Glib::RefPtr<Gst::Tee>        m_out_tee;
+};
---
+++ camera-pipes/pipeline/storage/framegrab_bin.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "pipeline/GST_element_base.hpp"
+
+#include <gstreamermm/caps.h>
+#include <gstreamermm/capsfilter.h>
+#include <gstreamermm/queue.h>
+
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+class framegrab_bin : public GST_element_base
+{
+public:
+  framegrab_bin();
+
+  void add_to_bin(const Glib::RefPtr<Gst::Bin>& bin) override;
+
+  bool link_front(const Glib::RefPtr<Gst::Element>& node) override;
+
+  bool init(const char name[]) override;
+
+  Glib::RefPtr<Gst::Element> front() override
+  {
+    return m_in_queue;
+  }
+
+  Glib::RefPtr<Gst::Bin> get_bin()
+  {
+    return m_bin;
+  }  
+
+protected:
+
+  Glib::RefPtr<Gst::Bin>        m_bin;
+
+  Glib::RefPtr<Gst::Queue>      m_in_queue;
+  Glib::RefPtr<Gst::Element>    m_videorate;
+  Glib::RefPtr<Gst::Caps>       m_out_caps;
+  Glib::RefPtr<Gst::CapsFilter> m_out_capsfilter;
+  Glib::RefPtr<Gst::Element>    m_multifilesink;
+};
