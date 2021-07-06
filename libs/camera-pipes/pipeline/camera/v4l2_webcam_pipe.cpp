a397bd2c5d4fade39cf97e8a9dacd7e11d1480a8
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2021-07-06 16:05:31 -0700

    do-timestamp, is-live

--- libs/camera-pipes/pipeline/camera/v4l2_webcam_pipe.cpp
+++ libs/camera-pipes/pipeline/camera/v4l2_webcam_pipe.cpp
@@ -39,7 +39,8 @@ bool V4L2_webcam_pipe::init(const char name[])
 
     //source
     m_src = Gst::ElementFactory::create_element("v4l2src", name);
-    m_src->set_property("do_timestamp", true);
+    m_src->set_property("do-timestamp", true);
+    m_src->set_property("is-live", true);
     m_src->set_property("device", Glib::ustring("/dev/video2"));
     // m_src->add_probe(GST_PAD_PROBE_TYPE_IDLE | GST_PAD_PROBE_TYPE_EVENT_BOTH, sigc::mem_fun(&V4L2_webcam_pipe::on_pad_probe, this))
 
