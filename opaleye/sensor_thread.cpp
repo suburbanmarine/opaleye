04c8af795d682f7975167ba9676b6090d3f6a673
Jacob Schloss <jacob@schloss.io>
2021-07-13 12:06:12 -0700

    sensor thread

---
+++ opaleye/sensor_thread.cpp
@@ -0,0 +1,91 @@
+#include "sensor_thread.hpp"
+
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/bundled/printf.h>
+
+#include <thread>
+
+void thread_base::launch()
+{
+	m_thread = std::thread(&thread_base::work, this);
+}
+
+void thread_base::work()
+{
+
+}
+
+void thread_base::interrupt()
+{
+	m_keep_running = false;
+}
+
+//MT safe
+void thread_base::join()
+{
+	m_thread.join();
+}
+
+bool sensor_thread::init()
+{
+	m_i2c = std::make_shared<i2c_linux>();
+	bool ret = m_i2c->init("/dev/i2c-0");
+	if(!ret)
+	{
+	    SPDLOG_ERROR("I2C init failed");
+		return false;
+	}
+
+	ret = m_temp.reset();
+	if(!ret)
+	{
+		SPDLOG_ERROR("temp reset failed");
+		return false;
+	}
+
+	ret = m_baro.reset();
+	if(!ret)
+	{
+		SPDLOG_ERROR("baro reset failed");
+		return false;
+	}
+
+
+	ret = m_temp.read_cal_data(&m_temp_cal_data);
+	if(!ret)
+	{
+		SPDLOG_ERROR("temp cal data failed");
+		return false;
+	}
+
+
+	ret = m_baro.read_cal_data(&m_baro_cal_data);
+	if(!ret)
+	{
+		SPDLOG_ERROR("baro cal data failed");
+		return false;
+	}
+
+	return true;
+}
+
+void sensor_thread::work()
+{	
+
+	while(m_keep_running)
+	{
+		bool ret = m_temp.sample();
+		if(!ret)
+		{
+			SPDLOG_WARN("temp sample failed");
+		}
+
+		ret = m_baro.sample();
+		if(!ret)
+		{
+			SPDLOG_WARN("baro sample failed");
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(5 * 1000));
+	}
+}
---
+++ opaleye/sensor_thread.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "MS5837-30BA.hpp"
+#include "TSYS01.hpp"
+
+#include <atomic>
+#include <thread>
+
+class thread_base
+{
+public:
+	thread_base() : m_keep_running(true)
+	{
+
+	}
+	virtual ~thread_base()
+	{
+
+	}
+
+	void launch();
+
+	virtual void work();
+
+	void interrupt();
+
+	//MT safe
+	void join();
+protected:
+	std::atomic<bool> m_keep_running;
+	std::thread m_thread;
+};
+
+class sensor_thread : public thread_base
+{
+public:
+
+  bool init();
+
+  void work() override;
+
+protected:
+	std::shared_ptr<i2c_linux> m_i2c;
+
+	TSYS01 m_temp;
+	TSYS01::CAL_DATA m_temp_cal_data;
+
+	MS5837_30BA m_baro;
+	MS5837_30BA::CAL_DATA m_baro_cal_data;
+
+};
