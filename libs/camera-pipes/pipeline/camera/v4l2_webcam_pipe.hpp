6721550ffdb487faf7a7512dee1c5291c89e4db3
Jacob Schloss <jacob@schloss.io>
2021-07-19 01:12:32 -0400

    add which

--- libs/camera-pipes/pipeline/camera/v4l2_webcam_pipe.hpp
+++ libs/camera-pipes/pipeline/camera/v4l2_webcam_pipe.hpp
@@ -62,13 +62,19 @@ protected:
   bool v4l2_ctrl_set(uint32_t id, const bool val);
   bool v4l2_ctrl_set(uint32_t id, const int32_t val);
   bool v4l2_ctrl_set(uint32_t id, const int64_t val);
+  bool v4l2_ctrl_set(uint32_t id, const uint8_t val);
+  bool v4l2_ctrl_set(uint32_t id, const uint16_t val);
+  bool v4l2_ctrl_set(uint32_t id, const uint32_t val);
 
-  bool v4l2_ctrl_get(uint32_t id, bool*    const out_val);
-  bool v4l2_ctrl_get(uint32_t id, int32_t* const out_val);
-  bool v4l2_ctrl_get(uint32_t id, int64_t* const out_val);
+  bool v4l2_ctrl_get(uint32_t id, uint32_t which, bool*    const out_val);
+  bool v4l2_ctrl_get(uint32_t id, uint32_t which, int32_t* const out_val);
+  bool v4l2_ctrl_get(uint32_t id, uint32_t which, int64_t* const out_val);
+  bool v4l2_ctrl_get(uint32_t id, uint32_t which, uint8_t* const out_val);
+  bool v4l2_ctrl_get(uint32_t id, uint32_t which, uint16_t* const out_val);
+  bool v4l2_ctrl_get(uint32_t id, uint32_t which, uint32_t* const out_val);
 
   bool v4l2_ctrl_set(v4l2_ext_control* const ctrl);
-  bool v4l2_ctrl_get(v4l2_ext_control* const ctrl);
+  bool v4l2_ctrl_get(uint32_t which, v4l2_ext_control* const ctrl);
 
   // bool on_bus_message(const Glib::RefPtr<Gst::Bus>& bus, const Glib::RefPtr<Gst::Message>& message);
   void on_bus_message(const Glib::RefPtr<Gst::Message>& message);
