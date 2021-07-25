2e6ba142bcd6808a81681d814ce870ae0418b4de
Jacob Schloss <jacob@schloss.io>
2021-07-24 22:41:56 -0400

    tweak build flags

--- generate_cmake_x86.sh
+++ generate_cmake_x86.sh
@@ -13,6 +13,11 @@ pushd build/release
 cmake -DCMAKE_BUILD_TYPE=Release $TOOLCHAIN $BASE_PATH
 popd
 
+mkdir -p build/relwdbg
+pushd build/relwdbg
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo $TOOLCHAIN $BASE_PATH
+popd
+
 mkdir -p build/debug
 pushd build/debug
 cmake -DCMAKE_BUILD_TYPE=Debug $TOOLCHAIN $BASE_PATH
