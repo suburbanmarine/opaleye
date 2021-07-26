798d7f6168fc5b93c01bc95e2e3d95afa18c37c3
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2021-07-26 16:26:44 -0700

    new sw api

---
+++ libs/camera-pipes/pipeline/Thumbnail_sw2_pipe.cpp
@@ -0,0 +1,174 @@
+#include "Thumbnail_sw2_pipe.hpp"
+
+#include "pipeline/gst_common.hpp"
+
+#include <gstreamermm/buffer.h>
+#include <gstreamermm/elementfactory.h>
+
+#include <spdlog/fmt/ostr.h>
+#include <spdlog/fmt/fmt.h>
+
+#include <opencv/core.hpp>
+#include <opencv/highgui.hpp>
+
+Thumbnail_sw2_pipe::Thumbnail_sw2_pipe()
+{
+
+}
+Thumbnail_sw2_pipe::~Thumbnail_sw2_pipe()
+{
+  if(m_thumb_jpeg_buffer_front)
+  {
+    free(m_thumb_jpeg_buffer_front);
+    m_thumb_jpeg_buffer_front = nullptr; 
+  }
+  if(m_thumb_jpeg_buffer_back)
+  {
+    free(m_thumb_jpeg_buffer_back);
+    m_thumb_jpeg_buffer_back = nullptr; 
+  }
+}
+
+void Thumbnail_sw2_pipe::add_to_bin(const Glib::RefPtr<Gst::Bin>& bin)
+{
+  bin->add(m_bin);
+}
+
+bool Thumbnail_sw2_pipe::link_front(const Glib::RefPtr<Gst::Element>& node)
+{
+  return false;
+}
+
+bool Thumbnail_sw2_pipe::init(const char name[])
+{
+  //init our internal bin and elements
+  {
+    m_bin = Gst::Bin::create(fmt::format("{:s}-bin", name).c_str());
+
+    //queue
+    m_in_queue = Gst::Queue::create();
+    // m_in_queue->set_property("leaky", Gst::QUEUE_LEAK_DOWNSTREAM);
+    // m_in_queue->property_min_threshold_time()    = 0;
+    // m_in_queue->property_min_threshold_buffers() = 0;
+    // m_in_queue->property_min_threshold_bytes()   = 0;
+    m_in_queue->property_max_size_buffers()      = 0;
+    m_in_queue->property_max_size_bytes()        = 0;
+    m_in_queue->property_max_size_time()         = 1000 * GST_MSECOND;
+
+    //out caps
+    m_appsink_caps = Gst::Caps::create_simple(
+      "image/jpeg",
+      "pixel-aspect-ratio", Gst::Fraction(1, 1),
+      // "format","JPG",
+      "width",              3840,
+      "height",             2160
+      );
+
+    m_appsink = Gst::AppSink::create();
+    m_appsink->property_caps().set_value(m_appsink_caps);
+    m_appsink->property_emit_signals() = true;
+    m_appsink->property_drop()         = true;
+    m_appsink->property_max_buffers()  = 1;
+    m_appsink->property_sync()         = false;
+
+    m_appsink->signal_new_sample().connect(
+      [this]()
+      {
+        handle_new_sample();
+        return Gst::FLOW_OK;
+      }
+    );
+
+    m_bin->add(m_in_queue);
+    m_bin->add(m_appsink);
+  }
+
+  m_in_queue->link(m_appsink);
+
+  m_frame_buffer = std::make_shared<std::vector<uint8_t>>();
+  m_frame_buffer->reserve(3480UL*2160UL*3UL);
+
+  m_thumb_jpeg_buffer_front = std::make_shared<std::vector<uint8_t>>();
+  m_thumb_jpeg_buffer_front->reserve(3480UL*2160UL*3UL);
+
+  m_thumb_jpeg_buffer_back = std::make_shared<std::vector<uint8_t>>();
+  m_thumb_jpeg_buffer_back->reserve(3480UL*2160UL*3UL);
+
+  m_stopwatch.set_alarm_dt(std::chrono::milliseconds(1000));
+
+  return true;
+}
+
+void Thumbnail_sw2_pipe::handle_new_sample()
+{
+  Glib::RefPtr<Gst::Sample> sample = m_appsink->try_pull_sample(0);
+  if(sample && m_stopwatch.is_expired())
+  {
+    m_stopwatch.reset();
+
+    Glib::RefPtr<Gst::Buffer> buffer = sample->get_buffer();
+
+    SPDLOG_INFO("Thumbnail_sw2_pipe::handle_new_sample has {}", buffer->get_size());
+    {
+      std::unique_lock<std::mutex> lock(m_frame_buffer_mutex);
+
+      m_frame_buffer->resize(buffer->get_size());
+      uint8_t* out_ptr = m_frame_buffer->data();
+
+      guint num = buffer->n_memory();
+      for(guint i = 0; i < num; i++)
+      {
+        Glib::RefPtr<Gst::Memory> mem_i = buffer->peek_memory(i);
+
+        Gst::MapInfo map_info;
+        mem_i->map(map_info, Gst::MAP_READ);
+
+        SPDLOG_INFO("Thumbnail_sw2_pipe::handle_new_sample block {} is {}", i, map_info.get_size());
+
+        guint8* blk_ptr = map_info.get_data();
+        gsize   blk_len = map_info.get_size();
+
+        std::copy_n(blk_ptr, blk_len, out_ptr);
+        out_ptr += blk_len;
+
+        mem_i->unmap(map_info);
+      }
+    }
+
+    if( ! downsample_jpeg() )
+    {
+       SPDLOG_ERROR("Thumbnail_sw2_pipe::downsample_jpeg failed");    
+    }
+  }
+  else
+  {
+    SPDLOG_INFO("Thumbnail_sw2_pipe::handle_new_sample has null sample"); 
+  }
+
+}
+
+bool Thumbnail_sw2_pipe::downsample_jpeg()
+{
+  int ret = 0;
+
+  {  
+    std::unique_lock<std::mutex> lock(m_frame_buffer_mutex);
+
+    //decompress full size frame
+    cv::Mat rawjpeg(1, m_frame_buffer.size(), CV_8UC1, m_frame_buffer.data());
+    cv::Mat decode_jpeg = cv::imdecode(rawjpeg, cv::IMREAD_COLOR);
+  }
+
+  //downsample
+
+  //encode
+
+
+  //flip pages
+  {  
+    std::unique_lock<std::mutex> lock(m_thumb_jpeg_mutex);
+    std::swap(m_thumb_jpeg_buffer_front, m_thumb_jpeg_buffer_back);
+  }
+
+  return true;
+}
---
+++ libs/camera-pipes/pipeline/Thumbnail_sw2_pipe.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "cameras/Logitech_brio.hpp"
+
+#include "pipeline/GST_element_base.hpp"
+#include "Thumbnail_sw_pipe.hpp"
+
+#include "Stopwatch.hpp"
+
+#include <spdlog/spdlog.h>
+
+#include <gstreamermm/appsink.h>
+#include <gstreamermm/caps.h>
+#include <gstreamermm/queue.h>
+
+#include <atomic>
+#include <memory>
+#include <mutex>
+
+/// NV accelerated but non-gstreamer thumbnail stream
+class Thumbnail_sw2_pipe : public Thumbnail_pipe_base
+{
+public:
+  Thumbnail_sw2_pipe();
+  ~Thumbnail_sw2_pipe() override;
+
+  void add_to_bin(const Glib::RefPtr<Gst::Bin>& bin) override;
+  bool link_front(const Glib::RefPtr<Gst::Element>& node) override;
+  bool link_back(const Glib::RefPtr<Gst::Element>& node) override
+  {
+    return false;
+  }
+
+  Glib::RefPtr<Gst::Element> front() override
+  {
+    return m_in_queue;
+  }
+
+  bool init(const char name[]) override;
+
+  void handle_new_sample();
+
+  void copy_frame_buffer(std::vector<uint8_t>* const out_frame_buffer)
+  {
+    {
+      std::unique_lock<std::mutex> lock(m_thumb_jpeg_mutex);
+      if(m_thumb_jpeg_buffer_front && out_frame_buffer)
+      {
+        out_frame_buffer->resize(m_thumb_jpeg_buffer_front_size);
+        std::copy_n(out_frame_buffer->begin(), m_thumb_jpeg_buffer_front_size, m_thumb_jpeg_buffer_front);
+      }
+      else
+      {
+        out_frame_buffer->clear();
+      }
+    }
+  }
+
+protected:
+
+  bool downsample_jpeg();
+
+  Glib::RefPtr<Gst::Bin>        m_bin;
+
+  Glib::RefPtr<Gst::Queue>      m_in_queue;
+
+  Glib::RefPtr<Gst::Caps>       m_appsink_caps;
+  Glib::RefPtr<Gst::AppSink>    m_appsink;
+
+  Stopwatch m_stopwatch;
+
+  //input jpeg from pipeline
+  mutable std::mutex m_frame_buffer_mutex;
+  std::shared_ptr<std::vector<uint8_t>> m_frame_buffer;
+
+  //output scaled down jpeg
+  mutable std::mutex m_thumb_jpeg_mutex;
+  std::shared_ptr<std::vector<uint8_t>> m_thumb_jpeg_buffer_front;
+  std::shared_ptr<std::vector<uint8_t>> m_thumb_jpeg_buffer_back;
+
+};
