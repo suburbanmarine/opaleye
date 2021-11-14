3ba82ce88756d093877e979c3a94f0f92ff85a28
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2021-11-13 16:09:10 -0800

    log req info

--- opaleye/http_req_callback_sensors.cpp
+++ opaleye/http_req_callback_sensors.cpp
@@ -11,6 +11,9 @@
 
 #include <Unit_conv.hpp>
 
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/bundled/printf.h>
 
@@ -23,6 +26,37 @@
 
 void http_req_callback_sensors::handle(FCGX_Request* const request)
 {
+  //TODO move this to base class
+  if(true)
+  { 
+    boost::property_tree::ptree temp;
+
+    temp.put<std::string>("QUERY_STRING",    FCGX_GetParam("QUERY_STRING",    request->envp));
+    temp.put<std::string>("REQUEST_METHOD",  FCGX_GetParam("REQUEST_METHOD",  request->envp));
+    temp.put<std::string>("CONTENT_TYPE",    FCGX_GetParam("CONTENT_TYPE",    request->envp));
+    temp.put<std::string>("CONTENT_LENGTH",  FCGX_GetParam("CONTENT_LENGTH",  request->envp));
+    temp.put<std::string>("SCRIPT_NAME",     FCGX_GetParam("SCRIPT_NAME",     request->envp));
+    temp.put<std::string>("REQUEST_URI",     FCGX_GetParam("REQUEST_URI",     request->envp));
+    temp.put<std::string>("DOCUMENT_URI",    FCGX_GetParam("DOCUMENT_URI",    request->envp));
+    temp.put<std::string>("DOCUMENT_ROOT",   FCGX_GetParam("DOCUMENT_ROOT",   request->envp));
+    temp.put<std::string>("SERVER_PROTOCOL", FCGX_GetParam("SERVER_PROTOCOL", request->envp));
+    temp.put<std::string>("REQUEST_SCHEME",  FCGX_GetParam("REQUEST_SCHEME",  request->envp));
+    char const * const HTTPS = FCGX_GetParam("HTTPS", request->envp);
+    temp.put<std::string>("HTTPS", (HTTPS) ? (HTTPS) : ("<null>"));
+    temp.put<std::string>("GATEWAY_INTERFACE", FCGX_GetParam("GATEWAY_INTERFACE", request->envp));
+    temp.put<std::string>("SERVER_SOFTWARE",   FCGX_GetParam("SERVER_SOFTWARE",   request->envp));
+    temp.put<std::string>("REMOTE_ADDR",       FCGX_GetParam("REMOTE_ADDR",       request->envp));
+    temp.put<std::string>("REMOTE_PORT",       FCGX_GetParam("REMOTE_PORT",       request->envp));
+    temp.put<std::string>("SERVER_ADDR",       FCGX_GetParam("SERVER_ADDR",       request->envp));
+    temp.put<std::string>("SERVER_PORT",       FCGX_GetParam("SERVER_PORT",       request->envp));
+    temp.put<std::string>("SERVER_NAME",       FCGX_GetParam("SERVER_NAME",       request->envp));
+    temp.put<std::string>("REDIRECT_STATUS",   FCGX_GetParam("REDIRECT_STATUS",   request->envp));
+
+    std::stringstream ss;
+    boost::property_tree::write_json(ss, temp);
+    SPDLOG_DEBUG("request info: {:s", ss.str());
+  }
+
   //this is per-req since we could have several threads
   double ext_temp_data = 0.0;
   m_sensors->get_temp_data(&ext_temp_data);
--- opaleye/http_req_jpeg.cpp
+++ opaleye/http_req_jpeg.cpp
@@ -7,6 +7,9 @@
 #include "http_req_jpeg.hpp"
 #include "http_util.hpp"
 
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
 #include <spdlog/spdlog.h>
 
 #include <sys/types.h>
@@ -16,6 +19,37 @@
 
 void http_req_jpeg::handle(FCGX_Request* const request)
 {
+  //TODO move this to base class
+  if(true)
+  { 
+    boost::property_tree::ptree temp;
+
+    temp.put<std::string>("QUERY_STRING",    FCGX_GetParam("QUERY_STRING",    request->envp));
+    temp.put<std::string>("REQUEST_METHOD",  FCGX_GetParam("REQUEST_METHOD",  request->envp));
+    temp.put<std::string>("CONTENT_TYPE",    FCGX_GetParam("CONTENT_TYPE",    request->envp));
+    temp.put<std::string>("CONTENT_LENGTH",  FCGX_GetParam("CONTENT_LENGTH",  request->envp));
+    temp.put<std::string>("SCRIPT_NAME",     FCGX_GetParam("SCRIPT_NAME",     request->envp));
+    temp.put<std::string>("REQUEST_URI",     FCGX_GetParam("REQUEST_URI",     request->envp));
+    temp.put<std::string>("DOCUMENT_URI",    FCGX_GetParam("DOCUMENT_URI",    request->envp));
+    temp.put<std::string>("DOCUMENT_ROOT",   FCGX_GetParam("DOCUMENT_ROOT",   request->envp));
+    temp.put<std::string>("SERVER_PROTOCOL", FCGX_GetParam("SERVER_PROTOCOL", request->envp));
+    temp.put<std::string>("REQUEST_SCHEME",  FCGX_GetParam("REQUEST_SCHEME",  request->envp));
+    char const * const HTTPS = FCGX_GetParam("HTTPS", request->envp);
+    temp.put<std::string>("HTTPS", (HTTPS) ? (HTTPS) : ("<null>"));
+    temp.put<std::string>("GATEWAY_INTERFACE", FCGX_GetParam("GATEWAY_INTERFACE", request->envp));
+    temp.put<std::string>("SERVER_SOFTWARE",   FCGX_GetParam("SERVER_SOFTWARE",   request->envp));
+    temp.put<std::string>("REMOTE_ADDR",       FCGX_GetParam("REMOTE_ADDR",       request->envp));
+    temp.put<std::string>("REMOTE_PORT",       FCGX_GetParam("REMOTE_PORT",       request->envp));
+    temp.put<std::string>("SERVER_ADDR",       FCGX_GetParam("SERVER_ADDR",       request->envp));
+    temp.put<std::string>("SERVER_PORT",       FCGX_GetParam("SERVER_PORT",       request->envp));
+    temp.put<std::string>("SERVER_NAME",       FCGX_GetParam("SERVER_NAME",       request->envp));
+    temp.put<std::string>("REDIRECT_STATUS",   FCGX_GetParam("REDIRECT_STATUS",   request->envp));
+
+    std::stringstream ss;
+    boost::property_tree::write_json(ss, temp);
+    SPDLOG_DEBUG("request info: {:s", ss.str());
+  }
+
   //this is per-req since we could have several threads
   std::vector<uint8_t> frame_buf;
   m_cb(&frame_buf);
