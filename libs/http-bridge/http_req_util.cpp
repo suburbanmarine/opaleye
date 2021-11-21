1a529bbb45a0d1b55c7342427664319b362ebff8
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2021-11-21 01:19:17 -0800

    refactor req envp handling

--- libs/http-bridge/http_req_util.cpp
+++ libs/http-bridge/http_req_util.cpp
@@ -1,24 +1,24 @@
 http_req_util::http_req_util(FCGX_Request* const request)
 {
-	QUERY_STRING = "";
-	REQUEST_METHOD = "";
-	CONTENT_TYPE = "";
-	CONTENT_LENGTH = "";
-	SCRIPT_NAME = "";
-	REQUEST_URI = "";
-	DOCUMENT_URI = "";
-	DOCUMENT_ROOT = "";
-	SERVER_PROTOCOL = "";
-	REQUEST_SCHEME = "";
-	HTTPS = "";
+	QUERY_STRING      = "";
+	REQUEST_METHOD    = "";
+	CONTENT_TYPE      = "";
+	CONTENT_LENGTH    = "";
+	SCRIPT_NAME       = "";
+	REQUEST_URI       = "";
+	DOCUMENT_URI      = "";
+	DOCUMENT_ROOT     = "";
+	SERVER_PROTOCOL   = "";
+	REQUEST_SCHEME    = "";
+	HTTPS             = "";
 	GATEWAY_INTERFACE = "";
-	SERVER_SOFTWARE = "";
-	REMOTE_ADDR = "";
-	REMOTE_PORT = "";
-	SERVER_ADDR = "";
-	SERVER_PORT = "";
-	SERVER_NAME = "";
-	REDIRECT_STATUS = "";
+	SERVER_SOFTWARE   = "";
+	REMOTE_ADDR       = "";
+	REMOTE_PORT       = "";
+	SERVER_ADDR       = "";
+	SERVER_PORT       = "";
+	SERVER_NAME       = "";
+	REDIRECT_STATUS   = "";
 }
 http_req_util::~http_req_util()
 {
@@ -27,26 +27,26 @@ http_req_util::~http_req_util()
 
 void http_req_util::load(FCGX_Request* const request)
 {
-	QUERY_STRING      = QUERY_STRING =FCGX_GetParam("QUERY_STRING",    request->envp);
-	REQUEST_METHOD    = REQUEST_METHOD =FCGX_GetParam("REQUEST_METHOD",  request->envp);
-	CONTENT_TYPE      = CONTENT_TYPE =FCGX_GetParam("CONTENT_TYPE",    request->envp);
-	CONTENT_LENGTH    = CONTENT_LENGTH =FCGX_GetParam("CONTENT_LENGTH",  request->envp);
-	SCRIPT_NAME       = SCRIPT_NAME =FCGX_GetParam("SCRIPT_NAME",     request->envp);
-	REQUEST_URI       = REQUEST_URI =FCGX_GetParam("REQUEST_URI",     request->envp);
-	DOCUMENT_URI      = DOCUMENT_URI =FCGX_GetParam("DOCUMENT_URI",    request->envp);
-	DOCUMENT_ROOT     = DOCUMENT_ROOT =FCGX_GetParam("DOCUMENT_ROOT",   request->envp);
-	SERVER_PROTOCOL   = SERVER_PROTOCOL =FCGX_GetParam("SERVER_PROTOCOL", request->envp);
-	REQUEST_SCHEME    = REQUEST_SCHEME =FCGX_GetParam("REQUEST_SCHEME",  request->envp);
-	HTTPS             = FCGX_GetParam("HTTPS", request->envp);
+	QUERY_STRING      = FCGX_GetParam("QUERY_STRING",    request->envp);
+	REQUEST_METHOD    = FCGX_GetParam("REQUEST_METHOD",  request->envp);
+	CONTENT_TYPE      = FCGX_GetParam("CONTENT_TYPE",    request->envp);
+	CONTENT_LENGTH    = FCGX_GetParam("CONTENT_LENGTH",  request->envp);
+	SCRIPT_NAME       = FCGX_GetParam("SCRIPT_NAME",     request->envp);
+	REQUEST_URI       = FCGX_GetParam("REQUEST_URI",     request->envp);
+	DOCUMENT_URI      = FCGX_GetParam("DOCUMENT_URI",    request->envp);
+	DOCUMENT_ROOT     = FCGX_GetParam("DOCUMENT_ROOT",   request->envp);
+	SERVER_PROTOCOL   = FCGX_GetParam("SERVER_PROTOCOL", request->envp);
+	REQUEST_SCHEME    = FCGX_GetParam("REQUEST_SCHEME",  request->envp);
+	HTTPS             = FCGX_GetParam("HTTPS",           request->envp);
 	HTTPS             = (HTTPS) ? (HTTPS) : ("<null>");
-	GATEWAY_INTERFACE = GATEWAY_INTERFACE =FCGX_GetParam("GATEWAY_INTERFACE", request->envp);
-	SERVER_SOFTWARE   = SERVER_SOFTWARE =FCGX_GetParam("SERVER_SOFTWARE",   request->envp);
-	REMOTE_ADDR       = REMOTE_ADDR =FCGX_GetParam("REMOTE_ADDR",       request->envp);
-	REMOTE_PORT       = REMOTE_PORT =FCGX_GetParam("REMOTE_PORT",       request->envp);
-	SERVER_ADDR       = SERVER_ADDR =FCGX_GetParam("SERVER_ADDR",       request->envp);
-	SERVER_PORT       = SERVER_PORT =FCGX_GetParam("SERVER_PORT",       request->envp);
-	SERVER_NAME       = SERVER_NAME =FCGX_GetParam("SERVER_NAME",       request->envp);
-	REDIRECT_STATUS   = REDIRECT_STATUS =FCGX_GetParam("REDIRECT_STATUS",   request->envp);
+	GATEWAY_INTERFACE = FCGX_GetParam("GATEWAY_INTERFACE", request->envp);
+	SERVER_SOFTWARE   = FCGX_GetParam("SERVER_SOFTWARE",   request->envp);
+	REMOTE_ADDR       = FCGX_GetParam("REMOTE_ADDR",       request->envp);
+	REMOTE_PORT       = FCGX_GetParam("REMOTE_PORT",       request->envp);
+	SERVER_ADDR       = FCGX_GetParam("SERVER_ADDR",       request->envp);
+	SERVER_PORT       = FCGX_GetParam("SERVER_PORT",       request->envp);
+	SERVER_NAME       = FCGX_GetParam("SERVER_NAME",       request->envp);
+	REDIRECT_STATUS   = FCGX_GetParam("REDIRECT_STATUS",   request->envp);
 
 	doc_uri = DOCUMENT_URI;
 }
@@ -64,7 +64,7 @@ void http_req_util::log_request_env()
     request_env.put<std::string>("DOCUMENT_ROOT",     DOCUMENT_ROOT);
     request_env.put<std::string>("SERVER_PROTOCOL",   SERVER_PROTOCOL);
     request_env.put<std::string>("REQUEST_SCHEME",    REQUEST_SCHEME);
-    request_env.put<std::string>("HTTPS", (HTTPS) ? (HTTPS) : ("<null>"));
+    request_env.put<std::string>("HTTPS",             HTTPS);
     request_env.put<std::string>("GATEWAY_INTERFACE", GATEWAY_INTERFACE);
     request_env.put<std::string>("SERVER_SOFTWARE",   SERVER_SOFTWARE);
     request_env.put<std::string>("REMOTE_ADDR",       REMOTE_ADDR);
@@ -73,7 +73,6 @@ void http_req_util::log_request_env()
     request_env.put<std::string>("SERVER_PORT",       SERVER_PORT);
     request_env.put<std::string>("SERVER_NAME",       SERVER_NAME);
     request_env.put<std::string>("REDIRECT_STATUS",   REDIRECT_STATUS);
-
 
     boost::property_tree::ptree temp;
 	temp.put_child("env", QUERY_STRING);
