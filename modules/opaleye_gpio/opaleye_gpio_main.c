939617c5ebcdbd9010d7dc55409969a8fc7615e1
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2021-11-24 11:50:47 -0800

    gpio

--- modules/opaleye_gpio/opaleye_gpio_main.c
+++ modules/opaleye_gpio/opaleye_gpio_main.c
@@ -12,6 +12,17 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/timekeeping.h>
+#include <linux/gpio.h>
+
+//xavier
+#include <linux/dt-bindings/gpio/tegra194-gpio.h>
+#define TEGRA194_BASE 288
+
+//tx2
+// #include <linux/dt-bindings/gpio/tegra186-gpio.h>
+
+//nano
+// #include <linux/dt-bindings/gpio/tegra-gpio.h>
 
 // for sched_setscheduler
 // #include <linux/sched.h>
@@ -27,6 +38,15 @@ typedef struct opaleye_gpio_state_t
 {
 	struct task_struct* main_task_ptr;
 	struct hrtimer gpio_timer;
+
+	unsigned GPIO13_N01;
+	int GPIO13_N01_val;
+	unsigned GPIO01_Q05;
+	int GPIO01_Q05_val;
+	unsigned GPIO11_Q06;
+	int GPIO11_Q06_val;
+	unsigned GPIO09_R00;
+	int GPIO09_R00_val;
 } opaleye_gpio_state_t;
 
 static struct opaleye_gpio_state_t* g_gpio_state;
@@ -42,6 +62,51 @@ int __init opaleye_gpio_init(void)
 		return -1;
 	}
 
+	// get gpios
+	state->GPIO13_N01 = get_tegra194_gpio(TEGRA194_BASE, TEGRA194_MAIN_GPIO_PORT_N, 1);
+	int ret = gpio_request_one(GPIO13_N01, GPIOF_DIR_OUT | GPIOF_INIT_LOW, "opaleye_gpio_pin13");
+	if(ret != 0)
+	{
+		printk(KERN_ERR "gpio_request_one failed to start GPIO13_N01");
+
+		kfree(state);
+		state = NULL;
+		return -1;
+	}
+
+	state->GPIO01_Q05 = get_tegra194_gpio(TEGRA194_BASE, TEGRA194_MAIN_GPIO_PORT_Q, 5);
+	ret = gpio_request_one(GPIO01_Q05, GPIOF_DIR_OUT | GPIOF_INIT_LOW, "opaleye_gpio_pin1");
+	if(ret != 0)
+	{
+		printk(KERN_ERR "gpio_request_one failed to start GPIO01_Q05");
+
+		kfree(state);
+		state = NULL;
+		return -1;
+	}
+
+	state->GPIO11_Q06 = get_tegra194_gpio(TEGRA194_BASE, TEGRA194_MAIN_GPIO_PORT_Q, 6);
+	ret = gpio_request_one(GPIO11_Q06, GPIOF_DIR_OUT | GPIOF_INIT_LOW, "opaleye_gpio_pin11");
+	if(ret != 0)
+	{
+		printk(KERN_ERR "gpio_request_one failed to start GPIO11_Q06");
+
+		kfree(state);
+		state = NULL;
+		return -1;
+	}
+
+	state->GPIO09_R00 = get_tegra194_gpio(TEGRA194_BASE, TEGRA194_MAIN_GPIO_PORT_R, 0);
+	ret = gpio_request_one(GPIO09_R00, GPIOF_DIR_OUT | GPIOF_INIT_LOW, "opaleye_gpio_pin9");
+	if(ret != 0)
+	{
+		printk(KERN_ERR "gpio_request_one failed to start GPIO09_R00");
+
+		kfree(state);
+		state = NULL;
+		return -1;
+	}
+
 	// or to pin to a cpu
 	// kthread_create
 	// kthread_bind
@@ -51,8 +116,8 @@ int __init opaleye_gpio_init(void)
 	{
 		printk(KERN_ERR "opaleye_gpio failed to start thread");
 
-		kfree(g_gpio_state);
-		g_gpio_state = NULL;
+		kfree(state);
+		state = NULL;
 		return -1;
 	}
 
@@ -78,12 +143,21 @@ void __exit opaleye_gpio_exit(void)
 enum hrtimer_restart opaleye_gpio_timer_cb(struct hrtimer* t)
 {
 	opaleye_gpio_state_t* state = container_of(t, opaleye_gpio_state_t, gpio_timer);
-	hrtimer_forward_now(&state->gpio_timer, ktime_set(1, 0));
+	// hrtimer_forward_now(&state->gpio_timer, ktime_set(1, 0)); // 1s
+	hrtimer_forward_now(&state->gpio_timer, ktime_set(0, 500 * 1000 * 1000)); // 500ms
 
 	//do something, toggle a pin
+	gpio_set_value(state->GPIO13_N01, state->GPIO13_N01_val);
+	state->GPIO13_N01_val = ! state->GPIO13_N01_val;
 
 	// return HRTIMER_NORESTART;
 	return HRTIMER_RESTART;
+}
+
+// https://github.com/torvalds/linux/blob/master/include/dt-bindings/gpio/tegra194-gpio.h
+unsigned get_tegra194_gpio(int base, int port, int offset)
+{
+	return base + port*8 + offset;
 }
 
 int opaleye_gpio_main(void* data)
