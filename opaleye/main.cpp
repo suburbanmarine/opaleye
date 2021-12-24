d90378a6795f69f3780931b7e74bb3292fe5f936
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2021-12-24 02:35:43 -0500

    give dir to sensors

--- opaleye/main.cpp
+++ opaleye/main.cpp
@@ -159,6 +159,7 @@ int main(int argc, char* argv[])
 	std::shared_ptr<http_req_callback_sensors> sensor_cb = std::make_shared<http_req_callback_sensors>();
 	sensor_cb->init(sensors);
 	fcgi_svr.register_cb_for_doc_uri("/api/v1/sensors", sensor_cb);
+	fcgi_svr.register_cb_for_doc_uri("/api/v1/sensors/", sensor_cb);
 
 	SPDLOG_INFO("Starting fcgi connection");
 	fcgi_svr.start();
