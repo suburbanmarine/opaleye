cb04743f36628e08fe1c87381f8c5139acc0d067
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2022-01-23 17:05:36 -0800

    decode multipart correctly

--- tests/opaleye_zeromq.cpp
+++ tests/opaleye_zeromq.cpp
@@ -8,7 +8,7 @@
 int main()
 {
 
-	std::shared_ptr<zmq::context_t> m_context = m_context = std::make_shared<zmq::context_t>();
+	std::shared_ptr<zmq::context_t> m_context = std::make_shared<zmq::context_t>();
 
 	const std::string endpoint = "tcp://127.0.0.1:50000";
 	std::shared_ptr<zmq::socket_t> m_socket  = std::make_shared<zmq::socket_t>(*m_context, zmq::socket_type::sub);
@@ -18,7 +18,16 @@ int main()
 	std::vector<zmq::message_t> rcv_msgs;
 	for(size_t i = 0; i < 10; i++)
 	{
+		rcv_msgs.clear();
 		zmq::recv_multipart(*m_socket, std::back_inserter(rcv_msgs));
+
+		std::cout << "Got message with " << rcv_msgs.size() << " parts:\n";
+		for(size_t i = 0; i < rcv_msgs.size(); i++)
+		{
+			// std::cout << "\tPart " << i << ": " << std::string(rcv_msgs[i].data<char>(), rcv_msgs[i].size());
+			std::cout << "\tPart " << i << ": " << rcv_msgs[i].to_string();
+			std::cout << std::endl;
+		}
 	}
 
 	return 0;
