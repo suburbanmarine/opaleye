93bf9f6b32c0ffd9f5738a887ad32f5ce79f4204
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2022-01-23 23:57:33 -0800

    check ptr

--- opaleye/main.cpp
+++ opaleye/main.cpp
@@ -200,7 +200,15 @@ int main(int argc, char* argv[])
 	if(app.m_config->camera_configs.count("cam0"))
 	{
 		std::shared_ptr<http_req_jpeg> jpg_cb = std::make_shared<http_req_jpeg>();
-		jpg_cb->set_get_image_cb(std::bind(&Thumbnail_pipe_base::copy_frame_buffer, app.m_pipelines["cam0"]->get_element<Thumbnail_pipe_base>("thumb_0").get(), std::placeholders::_1));
+
+		auto thumb_0 = app.m_pipelines["cam0"]->get_element<Thumbnail_pipe_base>("thumb_0");
+		if( ! thumb_0 )
+		{
+			SPDLOG_ERROR("Could not get element thumb_0");
+			return -1;
+		}
+
+		jpg_cb->set_get_image_cb(std::bind(&Thumbnail_pipe_base::copy_frame_buffer, thumb_0.get(), std::placeholders::_1));
 		fcgi_svr.register_cb_for_doc_uri("/cameras/cam0.jpg", jpg_cb);
 		fcgi_svr.register_cb_for_doc_uri("/api/v1/cameras/cam0/live/full", jpg_cb);
 		fcgi_svr.register_cb_for_doc_uri("/api/v1/cameras/cam0/live/thumb", jpg_cb);
@@ -209,7 +217,15 @@ int main(int argc, char* argv[])
 	if(app.m_config->camera_configs.count("cam1"))
 	{
 		std::shared_ptr<http_req_jpeg> jpg_cb = std::make_shared<http_req_jpeg>();
-		jpg_cb->set_get_image_cb(std::bind(&Thumbnail_pipe_base::copy_frame_buffer, app.m_pipelines["cam1"]->get_element<Thumbnail_pipe_base>("thumb_0").get(), std::placeholders::_1));
+		
+		auto thumb_0 = app.m_pipelines["cam1"]->get_element<Thumbnail_pipe_base>("thumb_0");
+		if( ! thumb_0 )
+		{
+			SPDLOG_ERROR("Could not get element thumb_0");
+			return -1;
+		}
+
+		jpg_cb->set_get_image_cb(std::bind(&Thumbnail_pipe_base::copy_frame_buffer, thumb_0.get(), std::placeholders::_1));
 		fcgi_svr.register_cb_for_doc_uri("/cameras/cam1.jpg", jpg_cb);
 		fcgi_svr.register_cb_for_doc_uri("/api/v1/cameras/cam1/live/full", jpg_cb);
 		fcgi_svr.register_cb_for_doc_uri("/api/v1/cameras/cam1/live/thumb", jpg_cb);
@@ -224,7 +240,13 @@ int main(int argc, char* argv[])
 		//the camera callbacks are called within the context of a gstreamer thread and should return promptly
 		if(app.m_config->camera_configs.count("cam0"))
 		{
-			std::shared_ptr<nvac_imx219_pipe> cam0 = app.m_pipelines["cam0"]->get_element<nvac_imx219_pipe>("cam0");
+			std::shared_ptr<nvac_imx219_pipe> cam0 = app.m_pipelines["cam0"]->get_element<nvac_imx219_pipe>("cam_0");
+			if( ! cam0 )
+			{
+				SPDLOG_ERROR("Could not get element cam0");
+				return -1;
+			}
+
 			cam0->set_framebuffer_callback(
 				[&zmq_svr](const std::shared_ptr<const std::vector<uint8_t>>& frame_ptr)
 				{
@@ -242,7 +264,13 @@ int main(int argc, char* argv[])
 
 		if(app.m_config->camera_configs.count("cam1"))
 		{
-			std::shared_ptr<nvac_imx219_pipe> cam1 = app.m_pipelines["cam1"]->get_element<nvac_imx219_pipe>("cam1");
+			std::shared_ptr<nvac_imx219_pipe> cam1 = app.m_pipelines["cam1"]->get_element<nvac_imx219_pipe>("cam_1");
+			if( ! cam1 )
+			{
+				SPDLOG_ERROR("Could not get element cam1");
+				return -1;
+			}
+
 			cam1->set_framebuffer_callback(
 				[&zmq_svr](const std::shared_ptr<const std::vector<uint8_t>>& frame_ptr)
 				{
