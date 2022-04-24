3e06c2cc41b5e239956061e249f91afed2bc8285
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2022-04-24 14:41:18 -0700

    new v4l ctrls

--- libs/camera-pipes/pipeline/camera/GST_v4l2_api.cpp
+++ libs/camera-pipes/pipeline/camera/GST_v4l2_api.cpp
@@ -1,20 +1,142 @@
 #include "GST_v4l2_api.hpp"
 
-bool GST_v4l2_api::set_exposure_mode(int32_t val)
+//User Controls
+bool GST_v4l2_api::set_brightness(int32_t val)
+{
+	return m_v4l2_util.v4l2_ctrl_set(V4L2_CID_BRIGHTNESS, val);
+}
+bool GST_v4l2_api::get_brightness(int32_t* const val)
+{
+	return m_v4l2_util.v4l2_ctrl_get(V4L2_CID_BRIGHTNESS, val);
+}
+
+bool GST_v4l2_api::set_gain(int32_t val)
+{
+	return m_v4l2_util.v4l2_ctrl_set(V4L2_CID_GAIN, val);
+}
+bool GST_v4l2_api::get_gain(int32_t* const val)
+{
+	return m_v4l2_util.v4l2_ctrl_get(V4L2_CID_GAIN, val);
+}
+
+bool GST_v4l2_api::set_gain_auto(bool val)
+{
+	return m_v4l2_util.v4l2_ctrl_set(V4L2_CID_AUTOGAIN, val);
+}
+bool GST_v4l2_api::get_gain_auto(bool* const val)
+{
+	return m_v4l2_util.v4l2_ctrl_get(V4L2_CID_AUTOGAIN, val);
+}
+
+//Camera Controls
+
+bool GST_v4l2_api::set_exposure_auto(int32_t val)
 {
 	return m_v4l2_util.v4l2_ctrl_set(V4L2_CID_EXPOSURE_AUTO, val);
 }
-bool GST_v4l2_api::get_exposure_mode(int32_t* const val)
+bool GST_v4l2_api::get_exposure_auto(int32_t* const val)
 {
 	return m_v4l2_util.v4l2_ctrl_get(V4L2_CID_EXPOSURE_AUTO, val);
 }
-bool GST_v4l2_api::set_exposure_value(int32_t val)
+bool GST_v4l2_api::set_exposure_absolute(int32_t val)
 {
 	return m_v4l2_util.v4l2_ctrl_set(V4L2_CID_EXPOSURE_ABSOLUTE, val);
 }
-bool GST_v4l2_api::get_exposure_value(int32_t* const val)
+bool GST_v4l2_api::get_exposure_absolute(int32_t* const val)
 {
 	return m_v4l2_util.v4l2_ctrl_get(V4L2_CID_EXPOSURE_ABSOLUTE, val);
+}
+
+bool GST_v4l2_api::set_exposure_auto_min(int32_t val)
+{
+	auto ctrl_id = m_v4l2_util.get_ctrl_id_by_name("exposure_auto_min");
+
+	if( ! ctrl_id )
+	{
+		return false;
+	}
+
+	return m_v4l2_util.v4l2_ctrl_set(ctrl_id.value(), val);
+}
+bool GST_v4l2_api::get_exposure_auto_min(int32_t* const val)
+{
+	auto ctrl_id = m_v4l2_util.get_ctrl_id_by_name("exposure_auto_min");
+
+	if( ! ctrl_id )
+	{
+		return false;
+	}
+
+	return m_v4l2_util.v4l2_ctrl_get(ctrl_id.value(), val);
+}
+
+bool GST_v4l2_api::set_exposure_auto_max(int32_t val)
+{
+	auto ctrl_id = m_v4l2_util.get_ctrl_id_by_name("exposure_auto_max");
+
+	if( ! ctrl_id )
+	{
+		return false;
+	}
+
+	return m_v4l2_util.v4l2_ctrl_set(ctrl_id.value(), val);
+}
+bool GST_v4l2_api::get_exposure_auto_max(int32_t* const val)
+{
+	auto ctrl_id = m_v4l2_util.get_ctrl_id_by_name("exposure_auto_max");
+
+	if( ! ctrl_id )
+	{
+		return false;
+	}
+
+	return m_v4l2_util.v4l2_ctrl_get(ctrl_id.value(), val);
+}
+
+bool GST_v4l2_api::set_gain_auto_min(int32_t val)
+{
+	auto ctrl_id = m_v4l2_util.get_ctrl_id_by_name("gain_auto_min");
+
+	if( ! ctrl_id )
+	{
+		return false;
+	}
+
+	return m_v4l2_util.v4l2_ctrl_set(ctrl_id.value(), val);
+}
+bool GST_v4l2_api::get_gain_auto_min(int32_t* const val)
+{
+	auto ctrl_id = m_v4l2_util.get_ctrl_id_by_name("gain_auto_min");
+
+	if( ! ctrl_id )
+	{
+		return false;
+	}
+
+	return m_v4l2_util.v4l2_ctrl_get(ctrl_id.value(), val);
+}
+
+bool GST_v4l2_api::set_gain_auto_max(int32_t val)
+{
+	auto ctrl_id = m_v4l2_util.get_ctrl_id_by_name("gain_auto_max");
+
+	if( ! ctrl_id )
+	{
+		return false;
+	}
+
+	return m_v4l2_util.v4l2_ctrl_set(ctrl_id.value(), val);
+}
+bool GST_v4l2_api::get_gain_auto_max(int32_t* const val)
+{
+	auto ctrl_id = m_v4l2_util.get_ctrl_id_by_name("gain_auto_max");
+
+	if( ! ctrl_id )
+	{
+		return false;
+	}
+
+	return m_v4l2_util.v4l2_ctrl_get(ctrl_id.value(), val);
 }
 
 bool GST_v4l2_api::set_focus_absolute(int32_t val)
@@ -35,20 +157,4 @@ bool GST_v4l2_api::get_focus_auto(bool* const val)
 	return m_v4l2_util.v4l2_ctrl_get(V4L2_CID_FOCUS_AUTO, val);
 }
 
-bool GST_v4l2_api::set_brightness(int32_t val)
-{
-	return m_v4l2_util.v4l2_ctrl_set(V4L2_CID_BRIGHTNESS, val);
-}
-bool GST_v4l2_api::get_brightness(int32_t* const val)
-{
-	return m_v4l2_util.v4l2_ctrl_get(V4L2_CID_BRIGHTNESS, val);
-}
 
-bool GST_v4l2_api::set_gain(int32_t val)
-{
-	return m_v4l2_util.v4l2_ctrl_set(V4L2_CID_GAIN, val);
-}
-bool GST_v4l2_api::get_gain(int32_t* const val)
-{
-	return m_v4l2_util.v4l2_ctrl_get(V4L2_CID_GAIN, val);
-}
--- libs/camera-pipes/pipeline/camera/GST_v4l2_api.hpp
+++ libs/camera-pipes/pipeline/camera/GST_v4l2_api.hpp
@@ -17,11 +17,38 @@ public:
 
 	}
 
-  virtual bool set_exposure_mode(int32_t val);
-  virtual bool get_exposure_mode(int32_t* const val);
+//User Controls
+  // virtual bool set_exposure(int32_t val);
+  // virtual bool get_exposure(int32_t* const val);
 
-  virtual bool set_exposure_value(int32_t val);
-  virtual bool get_exposure_value(int32_t* const val);
+  virtual bool set_brightness(int32_t val);
+  virtual bool get_brightness(int32_t* const val);
+  
+  virtual bool set_gain(int32_t val);
+  virtual bool get_gain(int32_t* const val);
+
+  virtual bool set_gain_auto(bool val);
+  virtual bool get_gain_auto(bool* const val);
+
+//Camera Controls
+
+  virtual bool set_exposure_auto(int32_t val);
+  virtual bool get_exposure_auto(int32_t* const val);
+
+  virtual bool set_exposure_absolute(int32_t val);
+  virtual bool get_exposure_absolute(int32_t* const val);
+
+  virtual bool set_exposure_auto_min(int32_t val);
+  virtual bool get_exposure_auto_min(int32_t* const val);
+
+  virtual bool set_exposure_auto_max(int32_t val);
+  virtual bool get_exposure_auto_max(int32_t* const val);
+
+  virtual bool set_gain_auto_min(int32_t val);
+  virtual bool get_gain_auto_min(int32_t* const val);
+
+  virtual bool set_gain_auto_max(int32_t val);
+  virtual bool get_gain_auto_max(int32_t* const val);
 
   virtual bool set_focus_absolute(int32_t val);
   virtual bool get_focus_absolute(int32_t* const val);
@@ -29,11 +56,6 @@ public:
   virtual bool set_focus_auto(bool val);
   virtual bool get_focus_auto(bool* const val);
 
-  virtual bool set_brightness(int32_t val);
-  virtual bool get_brightness(int32_t* const val);
-
-  virtual bool set_gain(int32_t val);
-  virtual bool get_gain(int32_t* const val);
 protected:
     v4l2_util m_v4l2_util;
 };
