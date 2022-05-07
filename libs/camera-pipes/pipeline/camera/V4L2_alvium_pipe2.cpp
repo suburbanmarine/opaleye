7da579d41f5cc34b4c63e285ca0d3637da457530
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2022-05-07 01:07:31 -0700

    gstreamermm is buggy

--- libs/camera-pipes/pipeline/camera/V4L2_alvium_pipe2.cpp
+++ libs/camera-pipes/pipeline/camera/V4L2_alvium_pipe2.cpp
@@ -17,6 +17,10 @@
 #include <gst/video/video-format.h>
 #include <gst/video/gstvideometa.h>
 
+#include <gst/gst.h>
+#include <gst/app/gstappsrc.h>
+#include <gst/app/gstappsink.h>
+
 #include <rapidjson/document.h>
 #include <rapidjson/writer.h>
 #include <rapidjson/stringbuffer.h>
@@ -32,7 +36,7 @@
 
 V4L2_alvium_pipe2::V4L2_alvium_pipe2() : m_gst_need_data(false)
 {
-  
+  t0 = 0;
 }
 V4L2_alvium_pipe2::~V4L2_alvium_pipe2()
 {
@@ -81,18 +85,18 @@ bool V4L2_alvium_pipe2::init(const char name[])
 {
 
   m_bin = Gst::Bin::create(fmt::format("{:s}-bin", name).c_str());
-
-  m_src_caps = Glib::wrap(gst_caps_new_simple ("video/x-raw",
-       "format", G_TYPE_STRING, "RGB",
-       "framerate", GST_TYPE_FRACTION, 20, 1,
-       "max-framerate", GST_TYPE_FRACTION, 20, 1,
-       "pixel-aspect-ratio", GST_TYPE_FRACTION, 1, 1,
-       "width", G_TYPE_INT, 1920,
-       "height", G_TYPE_INT, 1080,
-       "interlace-mode", G_TYPE_STRING, "progressive",
-       // "colorimetry", G_TYPE_STRING, "sRGB",
-       NULL));
-
+#if 0
+  // m_src_caps = Glib::wrap(gst_caps_new_simple ("video/x-raw",
+  //      "format", G_TYPE_STRING, "RGB",
+  //      "framerate", GST_TYPE_FRACTION, 20, 1,
+  //      "max-framerate", GST_TYPE_FRACTION, 20, 1,
+  //      "pixel-aspect-ratio", GST_TYPE_FRACTION, 1, 1,
+  //      "width", G_TYPE_INT, 1920,
+  //      "height", G_TYPE_INT, 1080,
+  //      "interlace-mode", G_TYPE_STRING, "progressive",
+  //      // "colorimetry", G_TYPE_STRING, "sRGB",
+  //      NULL));
+    m_src_caps = Gst::Caps::create_from_string("video/x-raw, format=RGB, framerate=20/1, max-framerate=20/1, pixel-aspect-ratio=1/1, width=1920, height=1080, interlace-mode=progressive, colorimetry=sRGB");
 
     if(! m_src_caps )
     {
@@ -107,11 +111,8 @@ bool V4L2_alvium_pipe2::init(const char name[])
     m_src->property_is_live()      = true;
     m_src->property_do_timestamp() = true;
     m_src->property_block()        = false;
-    // m_src->property_block()        = true; // TODO: this may need to be true to enable internal buffer
-    m_src->property_min_latency()  = 0;
-    m_src->property_max_latency()  = 1*GST_SECOND / 2;
 
-    m_src->property_num_buffers()  = 300;
+    m_src->property_num_buffers()  = 5;
     m_src->property_max_bytes()    = 0;
 
     // m_src->property_emit_signals() = false;
@@ -128,14 +129,21 @@ bool V4L2_alvium_pipe2::init(const char name[])
     // m_src->signal_seek_data().connect(
     //   [this](guint64 val){return handle_seek_data(val);}
     //   );
-    
+    #else
+    m_src = Glib::wrap(GST_APP_SRC(gst_element_factory_make("appsrc", NULL)));
+    GstCaps* caps = gst_caps_from_string("video/x-raw, format=RGB, framerate=0/1, max-framerate=20/1, pixel-aspect-ratio=1/1, width=1920, height=1080, interlace-mode=progressive, colorimetry=sRGB");
+    gst_app_src_set_caps(GST_APP_SRC(m_src->gobj()), caps);
+    #endif
 
+    m_conv = Gst::ElementFactory::create_element("videoconvert");
     m_sink = Gst::ElementFactory::create_element("autovideosink");
 
     m_bin->add(m_src);
+    m_bin->add(m_conv);
     m_bin->add(m_sink);
 
-  m_src->link(m_sink);
+  m_src->link(m_conv);
+  m_conv->link(m_sink);
 
   m_thread = std::thread(std::bind(&V4L2_alvium_pipe2::new_frame_cb_XR24, this));
 
@@ -152,6 +160,25 @@ void V4L2_alvium_pipe2::handle_enough_data()
   // SPDLOG_ERROR("handle_enough_data");
   m_gst_need_data = false;
 }
+
+#if 1
+void V4L2_alvium_pipe2::new_frame_cb_XR24()
+{
+  while(true)
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    GstBuffer* buf = gst_buffer_new_and_alloc (1920*1080*3);
+    GstMapInfo map;
+    gst_buffer_map (buf, &map, GST_MAP_WRITE);
+    memset (map.data, 127, 1920*1080*3);
+    gst_buffer_unmap (buf, &map);
+
+    gst_app_src_push_buffer(GST_APP_SRC(m_src->gobj()), buf);
+  }
+}
+
+#else
 
 void V4L2_alvium_pipe2::new_frame_cb_XR24()
 {
@@ -185,12 +212,14 @@ void V4L2_alvium_pipe2::new_frame_cb_XR24()
       gsize offset[1] = {0};
       gint xstride = 1920 * 3;
       gint stride[1] = {xstride};
-      gst_buffer_add_video_meta_full(buf->gobj(), GST_VIDEO_FRAME_FLAG_NONE, GST_VIDEO_FORMAT_RGB, width, height, 1, offset, stride);
+      // gst_buffer_add_video_meta_full(buf->gobj(), GST_VIDEO_FRAME_FLAG_NONE, GST_VIDEO_FORMAT_RGB, width, height, 1, offset, stride);
+
+      // gst_buffer_add_video_meta(buf->gobj(), GST_VIDEO_FRAME_FLAG_NONE, GST_VIDEO_FORMAT_RGB, width, height);
 
       // GST_BUFFER_FLAG_SET(buf->gobj(), GST_BUFFER_FLAG_LIVE);
 
       std::copy_n(new_frame->data(), new_frame->size(), buf_map.get_data());
-      buf->unmap(buf_map);
+      // buf->unmap(buf_map);
      
       // gsize ins_len = buf->fill(0, frame_buf->get_data(), frame_buf->get_bytes_used());
       // if(ins_len != frame_buf->get_bytes_used())
@@ -198,9 +227,9 @@ void V4L2_alvium_pipe2::new_frame_cb_XR24()
       //   SPDLOG_ERROR("buffer did not accept all data");
       // }
 
-      // buf->set_pts(timeval_to_chrono(frame_buf->get_buf()->timestamp).count());
-      // buf->set_duration(GST_SECOND / 10);
-
+      buf->set_pts(t0);
+      buf->set_duration(GST_SECOND / 20);
+      t0 += GST_SECOND / 20;
       // m_curr_pts += std::chrono::nanoseconds(GST_SECOND / 30);
 
       // this used to be a problem - m_gst_need_data seems to help
@@ -237,3 +266,4 @@ void V4L2_alvium_pipe2::new_frame_cb_XR24()
   SPDLOG_DEBUG("V4L2_alvium_pipe2::new_frame_cb_XR24 - end");
   }
 }
+#endif
