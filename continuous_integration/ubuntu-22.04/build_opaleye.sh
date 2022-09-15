03fb438bcf489aaab9f5401685e144911cf9c9e8
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2022-09-14 23:07:47 -0700

    build scripts

--- continuous_integration/ubuntu-22.04/build_opaleye.sh
+++ continuous_integration/ubuntu-22.04/build_opaleye.sh
@@ -3,4 +3,5 @@ docker build -t opaleye-ubuntu-22.04 .
 CONTAINER_ID=$(docker create -it opaleye-ubuntu-22.04:latest /bin/bash)
 docker start $CONTAINER_ID
 docker cp $CONTAINER_ID:/root/zcm/build/zcm_1.1.5_amd64.deb .
+docker cp $CONTAINER_ID:/root/opaleye/build/release/opaleye-0.2.0-Linux.deb .
 docker stop $CONTAINER_ID
