eda69e22c7343e1118cff0e0d79abf9b3cc46a2b
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2022-09-14 20:04:12 -0700

    add script to get zcm installer

---
+++ continuous_integration/l4t-32.5.0/build_aarch64.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+trap 'exit -1' err
+
+# docker buildx create --name multibuilder --use
+# docker buildx build --platform linux/arm64 --output=type=registry -t suburbanmarine/sculpin_mavproxy:latest mavproxy_ubu20.04
+docker buildx build --platform linux/arm64 --output=type=registry -t suburbanmarine/opaleye:l4t-32.5.0 .
+
+# CONTAINER_ID=$(docker create -it sculpin_mavproxy:latest /bin/bash)
+
+# docker start $CONTAINER_ID
+
+# docker exec -w /root/mavlink $CONTAINER_ID git checkout 1.0.12
+# docker exec -w /root/mavlink $CONTAINER_ID git submodule sync
+# docker exec -w /root/mavlink $CONTAINER_ID git submodule update --init --recursive
+
+# docker exec -w /root/pymavlink $CONTAINER_ID git checkout 2.4.30
+# docker exec -w /root/pymavlink $CONTAINER_ID git submodule sync
+# docker exec -w /root/pymavlink $CONTAINER_ID git submodule update --init --recursive
+# docker exec -w /root/pymavlink -e MDEF=/root/mavlink/message_definitions $CONTAINER_ID python setup.py build install --user
+
+# docker exec -w /root/MAVProxy $CONTAINER_ID git checkout 1.8.50
+# docker exec -w /root/MAVProxy $CONTAINER_ID git submodule sync
+# docker exec -w /root/MAVProxy $CONTAINER_ID git submodule update --init --recursive
+# docker exec -w /root/MAVProxy $CONTAINER_ID python setup.py build install --user
+
+# docker stop $CONTAINER_ID
---
+++ continuous_integration/ubuntu-22.04/build_zcm.sh
@@ -0,0 +1,6 @@
+docker build -t opaleye-ubuntu-22.04 .
+
+CONTAINER_ID=$(docker create -it opaleye-ubuntu-22.04:latest /bin/bash)
+docker start $CONTAINER_ID
+docker cp $CONTAINER_ID:/root/zcm/build/zcm_1.1.5_amd64.deb .
+docker stop $CONTAINER_ID
