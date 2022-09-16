c93716ecb119055c1c03174d55aa25cad6d4442d
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2022-09-16 02:11:40 -0700

    use old docker for non-aarch64

--- continuous_integration/l4t-r32.6.1/build_opaleye.sh
+++ continuous_integration/l4t-r32.6.1/build_opaleye.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 trap 'exit -1' err
 
-docker buildx build --cache-to=type=local,dest=../cache --cache-from=type=local,src=../cache --platform linux/arm64 --output=type=image,push=false -t suburbanmarine/opaleye-ci:l4t-r32.6.1 .
+docker buildx build --cache-to=type=local,dest=../cache --cache-from=type=local,src=../cache --platform linux/arm64 --output=type=image,push=true -t suburbanmarine/opaleye-ci:l4t-r32.6.1 .
 
 CONTAINER_ID=$(docker create --rm -it suburbanmarine/opaleye-ci:l4t-r32.6.1 /bin/bash)
 
 docker start $CONTAINER_ID
 
 docker exec -w /root/zcm $CONTAINER_ID git checkout patch_installer
+docker exec -w /root/zcm $CONTAINER_ID git pull
 docker exec -w /root/zcm $CONTAINER_ID ./scripts/make_debian_package.sh
 docker exec -w /root/zcm $CONTAINER_ID gdebi -n /root/zcm/build/zcm_1.1.5_arm64.deb
 
 docker exec -w /root/opaleye               $CONTAINER_ID git checkout master
+docker exec -w /root/opaleye               $CONTAINER_ID git pull
 docker exec -w /root/opaleye               $CONTAINER_ID ./generate_cmake_xaviernx.sh
 docker exec -w /root/opaleye/build/release $CONTAINER_ID make -j`nproc`
 docker exec -w /root/opaleye/build/release $CONTAINER_ID make package
--- continuous_integration/ubuntu-22.04/build_opaleye.sh
+++ continuous_integration/ubuntu-22.04/build_opaleye.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 trap 'exit -1' err
 
-docker buildx build --cache-to=type=local,dest=../cache --cache-from=type=local,src=../cache --platform linux/amd64 --output=type=image,push=false -t suburbanmarine/opaleye-ci:22.04 .
+# docker buildx build --cache-to=type=local,dest=../cache --cache-from=type=local,src=../cache --platform linux/amd64 --output=type=image,push=true -t suburbanmarine/opaleye-ci:22.04 .
+docker build -t suburbanmarine/opaleye-ci:22.04 .
 
 CONTAINER_ID=$(docker create --rm -it suburbanmarine/opaleye-ci:22.04 /bin/bash)
 
 docker start $CONTAINER_ID
 
 docker exec -w /root/zcm $CONTAINER_ID git checkout patch_installer
+docker exec -w /root/zcm $CONTAINER_ID git pull
 docker exec -w /root/zcm $CONTAINER_ID ./scripts/make_debian_package.sh
 docker exec -w /root/zcm $CONTAINER_ID gdebi -n /root/zcm/build/zcm_1.1.5_amd64.deb
 
 docker exec -w /root/opaleye               $CONTAINER_ID git checkout master
+docker exec -w /root/opaleye               $CONTAINER_ID git pull
 docker exec -w /root/opaleye               $CONTAINER_ID ./generate_cmake_x86.sh
 docker exec -w /root/opaleye/build/release $CONTAINER_ID make -j`nproc`
 docker exec -w /root/opaleye/build/release $CONTAINER_ID make package
