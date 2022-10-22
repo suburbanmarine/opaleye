d18a038ac4e350e9ee25841cb2e64401ff0dfe2b
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2022-10-22 14:15:07 -0700

    tx2 toolchain

---
+++ Toolchain_Linux_tx2.cmake
@@ -0,0 +1,28 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_C_EXTENSIONS   OFF)
+
+set(FLAGS_ARCH  "-march=armv8-a+crypto -mcpu=cortex-a57+crypto")
+set(FLAGS_GEN   "-ffunction-sections -fdata-sections")
+set(FLAGS_WARN  "-Wall -Werror=return-type")
+
+set(CMAKE_ASM_FLAGS                "${FLAGS_ARCH} ${FLAGS_GEN} ${FLAGS_WARN} -Wa,--warn -x assembler-with-cpp" CACHE STRING "" FORCE)
+set(CMAKE_ASM_FLAGS_DEBUG           "-O0 -g3" CACHE STRING "" FORCE)
+set(CMAKE_ASM_FLAGS_RELEASE         "-Os"     CACHE STRING "" FORCE)
+set(CMAKE_ASM_FLAGS_RELWITHDEBINFO  "-Os -g3" CACHE STRING "" FORCE)
+set(CMAKE_ASM_FLAGS_MINSIZEREL      "-Os"     CACHE STRING "" FORCE)
+
+set(CMAKE_C_FLAGS                   "${FLAGS_ARCH} ${FLAGS_GEN} ${FLAGS_WARN}" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_DEBUG             "-O0 -g3" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_RELEASE           "-Os"     CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_RELWITHDEBINFO    "-Os -g3" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_MINSIZEREL        "-Os"     CACHE STRING "" FORCE)
+
+set(CMAKE_CXX_FLAGS                 "${FLAGS_ARCH} ${FLAGS_GEN} ${FLAGS_WARN}" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS_DEBUG           "-O0 -g3" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS_RELEASE         "-Os"     CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "-Os -g3" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS_MINSIZEREL      "-Os"     CACHE STRING "" FORCE)
