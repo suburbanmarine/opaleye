a2d008c010280cca0690363aa4a2a8cdde78a020
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2022-11-30 11:35:27 -0800

    insert as gray image

--- libs/camera-pipes/pipeline/camera/V4L2_imx183_pipe.cpp
+++ libs/camera-pipes/pipeline/camera/V4L2_imx183_pipe.cpp
@@ -317,7 +317,7 @@ bool V4L2_imx183_pipe::init(const char name[])
   {
     case PIX_FMT_RG12:
     {
-      m_src_caps= gst_caps_from_string("video/x-raw, format=rg12, framerate=0/1, max-framerate=20/1, pixel-aspect-ratio=1/1, width=2464, height=2056, interlace-mode=progressive, colorimetry=sRGB");
+      m_src_caps= gst_caps_from_string("video/x-raw, format=GRAY16_LE, framerate=0/1, max-framerate=20/1, pixel-aspect-ratio=1/1, width=5440, height=3648, interlace-mode=progressive, colorimetry=sRGB");
       break;
     }
     default:
@@ -463,7 +463,7 @@ void V4L2_imx183_pipe::new_frame_cb_RG12(const v4l2_base::ConstMmapFramePtr& fra
   }
   #endif
 
-  #if 0
+  #if 1
   {
     SPDLOG_DEBUG("feeding gst");
 
